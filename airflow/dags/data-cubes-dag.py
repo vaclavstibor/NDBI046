0e404f3f89c11917bd76205ac14b55c5e3d7e2ad
stiborv <vasa20017@seznam.cz>
2023-03-28 10:22:06 +0200

    Minor update

--- airflow/dags/data-cubes-dag.py
+++ airflow/dags/data-cubes-dag.py
@@ -2,9 +2,8 @@ from datetime import datetime
 from urllib import request
 import ssl
 
-from airflow import DAG
-
-from airflow.decorators import dag, task
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
 
 from operators.care_providers import care_providers_data_cube
 from operators.population import population_data_cube
@@ -20,47 +19,72 @@ CARE_PROVIDERS_CSV = "care-providers.csv"
 POPULATION_CSV = "population.csv"
 LAU_NUTS_MAP_CSV = "lau-nuts-map.csv"
 
+# @task(task_id="download-data-care-providers-csv")
+def download_data_care_providers():
+    print(f"Downloading... {CARE_PROVIDERS_CSV}")
+    request.urlretrieve(URL_CARE_PROVIDERS, DATA_DIR + CARE_PROVIDERS_CSV)
+    print("DONE")
+
+# @task(task_id="download-data-population-csv")
+def download_data_population():
+    print(f"Downloading... {POPULATION_CSV}")
+    request.urlretrieve(URL_POPULATION, DATA_DIR + POPULATION_CSV)
+    print("DONE")
+
+# @task(task_id="download-data-lau-nuts-map-csv")
+def download_data_lau_nuts_map():
+    print(f"Downloading... {LAU_NUTS_MAP_CSV}")
+    request.urlretrieve(URL_LAU_NUTS_MAP, DATA_DIR + LAU_NUTS_MAP_CSV)
+    print("DONE")
+
+# @task(task_id="create_care_providers_datacube")
+def create_care_providers_datacube(**kwargs):
+    output_dir = kwargs["dag_run"].conf.get("output_path", DATA_CUBES_DIR)
+    care_providers_data_cube()
+
+# @task(task_id="create_population_datacube")
+def create_population_datacube(**kwargs):
+    output_dir = kwargs["dag_run"].conf.get("output_path", DATA_CUBES_DIR)
+    population_data_cube()
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
-@dag(
-    dag_id="data-cubes",
+default_args = {
+    'start_date': datetime(year=2023, month=3, day=17)
+}
+
+with DAG(
+    dag_id='data_cubes',
+    default_args=default_args,
     schedule=None,
-    start_date=datetime(2023, 3, 27),
     catchup=False,
-    tags=["NDBI046"]
-)
+    description='Create data cubes'
+) as dag:
 
-def main():
-    @task(task_id="download-data-care-providers-csv")
-    def download_data_care_providers():
-        print(f"Downloading... {CARE_PROVIDERS_CSV}")
-        request.urlretrieve(URL_CARE_PROVIDERS, DATA_DIR + CARE_PROVIDERS_CSV)
-        print("DONE")
+    task_download_data_care_providers = PythonOperator(
+        task_id='download_data_care_providers_csv',
+        python_callable=download_data_care_providers
+    )
 
-    @task(task_id="download-data-population-csv")
-    def download_data_population():
-        print(f"Downloading... {POPULATION_CSV}")
-        request.urlretrieve(URL_POPULATION, DATA_DIR + POPULATION_CSV)
-        print("DONE")
+    task_download_data_population = PythonOperator(
+        task_id='download_data_population_csv',
+        python_callable=download_data_population
+    )
 
-    @task(task_id="download-data-lau-nuts-map-csv")
-    def download_data_lau_nuts_map():
-        print(f"Downloading... {LAU_NUTS_MAP_CSV}")
-        request.urlretrieve(URL_LAU_NUTS_MAP, DATA_DIR + LAU_NUTS_MAP_CSV)
-        print("DONE")
+    task_download_data_lau_nuts_map = PythonOperator(
+        task_id='download_data_lau_nuts_map_csv',
+        python_callable=download_data_lau_nuts_map
+    )
 
-    @task(task_id="create_care_providers_datacube")
-    def create_care_providers_datacube(**kwargs):
-        output_dir = kwargs["dag_run"].conf.get("output_path", DATA_CUBES_DIR)
-        care_providers_data_cube()
+    task_create_care_providers_datacube = PythonOperator(
+        task_id='create_care_providers_datacube',
+        python_callable=create_care_providers_datacube
+    )
 
-    @task(task_id="create_population_datacube")
-    def create_population_datacube(**kwargs):
-        output_dir = kwargs["dag_run"].conf.get("output_path", DATA_CUBES_DIR)
-        population_data_cube()
-
-    main >> download_data_care_providers() >> create_care_providers_datacube()
-    main >> [download_data_population(), download_data_lau_nuts_map()] >> create_population_datacube()
+    task_create_population_datacube = PythonOperator(
+        task_id='create_population_datacube',
+        python_callable=create_population_datacube
+    )
     
-if __name__ == "__main__":
-    main()
+    task_download_data_care_providers >> task_create_care_providers_datacube
+    [task_download_data_population, task_download_data_lau_nuts_map] >> task_create_population_datacube
